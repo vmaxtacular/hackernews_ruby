c1da87beb38e91c288ac0361c3456ffcee1d5b98
Veronica Maxwell <maxwellveronica1@gmail.com>
2017-01-27 19:00:28 +0800

    Added comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :submissions do
   	member do
-  		put: "like" to: "submission#upvote"
-  		put: "dislike" to: "submission#downvote"
+  		put "like", to: "submission#upvote"
+  		put "dislike", to: "submission#downvote"
   	end	
+  	resources :comments	
   end	
   
   root 'submissions#index'
